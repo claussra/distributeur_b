0bce0b87a96cb55a2e1fe247ebfa4ff1d1958eba
claussra <98280029+claussra@users.noreply.github.com>
2022-02-05 17:56:19 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,34 @@
 radio.onReceivedNumber(function (receivedNumber) {
     basic.showNumber(receivedNumber)
+    if (receivedNumber == 0) {
+        basic.showIcon(IconNames.Happy)
+        wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 90)
+        basic.pause(1000)
+        wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 35)
+        basic.pause(1000)
+        wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 35)
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                # . # . .
+                # # # # #
+                . . # . #
+                . # . # .
+                . # . # .
+                `)
+            basic.showLeds(`
+                . . # . #
+                # # # # #
+                # . # . .
+                . # . # .
+                . # . # .
+                `)
+        }
+        if (receivedNumber == 3) {
+            basic.showIcon(IconNames.Skull)
+            music.playMelody("C5 D C5 D C5 D C5 D ", 120)
+            basic.clearScreen()
+        }
+    }
 })
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(0)
@@ -10,7 +39,8 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(1)
 })
-input.onGesture(Gesture.Shake, function () {
+input.onGesture(Gesture.ThreeG, function () {
     radio.sendNumber(3)
 })
 radio.setGroup(156)
+wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 90)
